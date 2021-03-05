ce930ed4e04c0e9b8e70c919a03d7489af33d1f9
sms722 <56262896+sms722@users.noreply.github.com>
2021-03-04 20:33:31 -0500

    Add files via upload

---
+++ files_from_Srikar/Heuristics_2D/intersectHeuristic.m
@@ -0,0 +1,32 @@
+function intersectScore = intersectHeuristic(NC,CA_des)
+% This function computes the feasibility score for a design 
+% Inputs: nodal position matrix NC 
+%         Design Connectivity Array CA_des         
+    intersectScore = 1;
+    SortedCA = sortrows(CA_des);
+    
+    % Develop 4xM matrix of line segment endpoint coordinates, where M is 
+    %   the number of truss members.  Each row of format (x1,y1,x2,y2),
+    %   where point 1 is leftmost, point 2 is rightmost
+    PosA = [NC(SortedCA(:,1),1),NC(SortedCA(:,1),2),...
+            NC(SortedCA(:,2),1),NC(SortedCA(:,2),2)];
+    
+    % Loop through each pair of elements
+    for i = 1:1:size(PosA,1)
+        for j = 1:1:size(PosA,1)
+            % Determine whether the given pair of elements intersects
+            intersect = findLineSegIntersection([PosA(i,1),PosA(i,2)],...
+                        [PosA(i,3),PosA(i,4)],[PosA(j,1),PosA(j,2)],...
+                        [PosA(j,3),PosA(j,4)]);
+            
+            % Throw an error, given an intersection
+            if intersect == true
+                intersectScore = intersectScore - 0.1;
+                if intersectScore < 0.1
+                    return
+                end
+            end
+        end
+    end
+end
+
