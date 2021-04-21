9d3edfb511ab2723bd41410956b80e0b2ac79f89
sms722 <56262896+sms722@users.noreply.github.com>
2021-04-21 17:30:55 -0400

    Add files via upload

--- files_from_Srikar/Heuristics_2D/numMembersHeuristic_2D.m
+++ files_from_Srikar/Heuristics_2D/numMembersHeuristic_2D.m
@@ -2,12 +2,12 @@
 % This heuristic function identifies the number of members in each design,
 % and scores the design relative to a threshold representative of the
 % minimum number of members desired for stable designs
-function numMemScore = numMembersHeuristic_2D(CA,threshold)
+function numMemScore = numMembersHeuristic_2D(CA,threshold,totPosNum)
     numMems = size(CA,1);
-    if threshold > 18
+    if threshold > (totPosNum/2)
         div = 1/abs(threshold);
     else
-        div = 1/abs(threshold - 36);
+        div = 1/abs(threshold - totPosNum);
     end
     numMemScore = round((1-(0.1*div*abs(threshold-numMems))),1);
 end
