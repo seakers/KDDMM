d485f12580a5bc4747e25c6e55b6e7d0a71b871c
sms722 <56262896+sms722@users.noreply.github.com>
2021-05-20 16:41:43 -0400

    Add files via upload

---
+++ files_from_Srikar/Constraints_2D/feasibility_checker_nonbinary_V3.m
@@ -0,0 +1,93 @@
+function [feasibilityScore] = feasibility_checker_nonbinary_V3(NC,CA_des)
+% This function computes the feasibility score for a design 
+% Inputs: nodal position matrix NC 
+%         Design Connectivity Array CA_des         
+    feasibilityScore = 1;
+
+    % FIRST CONSTRAINT: members only intersect at nodes (no crossing)
+    % Sort points from left to right by x-position
+    SortedCA = sortrows(CA_des);
+    
+    % Develop 4xM matrix of line segment endpoint coordinates, where M is 
+    %   the number of truss members.  Each row of format (x1,y1,x2,y2),
+    %   where point 1 is leftmost, point 2 is rightmost
+    PosA = [NC(SortedCA(:,1),1),NC(SortedCA(:,1),2),...
+            NC(SortedCA(:,2),1),NC(SortedCA(:,2),2)];
+    
+    % Loop through each pair of elements
+    for i = 1:1:size(PosA,1)
+        for j = 1:1:size(PosA,1)
+            % Determine whether the given pair of elements intersects
+            intersect = findLineSegIntersection([PosA(i,1),PosA(i,2)],...
+                        [PosA(i,3),PosA(i,4)],[PosA(j,1),PosA(j,2)],...
+                        [PosA(j,3),PosA(j,4)]);
+            
+            % Throw an error, given an intersection
+            if intersect == true
+                feasibilityScore = feasibilityScore - 0.1;
+                if feasibilityScore < 0.1
+                    return
+                end
+            end
+        end
+    end
+    
+    % SECOND CONSTRAINT: Elements (of either the same or different lengths)
+    %   cannot overlap
+    % Loop through each element
+    for k = 1:1:size(SortedCA,1)
+        % Loop through each element again, to consider each possible pair 
+        %   of elements
+        for q = 1:1:size(SortedCA,1)
+            % Check if both elements share a common startpoint
+            if (NC(SortedCA(k,1),1) == NC(SortedCA(q,1),1)) && ...
+                (NC(SortedCA(k,1),2) == NC(SortedCA(q,1),2))
+                % Check if both elements have the same slope (and reject 
+                %    the design if so)
+                mk = (NC(SortedCA(k,2),2)-NC(SortedCA(k,1),2))/...
+                     (NC(SortedCA(k,2),1)-NC(SortedCA(k,1),1));
+                mq = (NC(SortedCA(q,2),2)-NC(SortedCA(q,1),2))/...
+                     (NC(SortedCA(q,2),1)-NC(SortedCA(q,1),1));
+                % If the same element is being compared twice, move on
+                if k == q
+                    continue
+                elseif mk == mq
+                   feasibilityScore = feasibilityScore - 0.1;
+                   if feasibilityScore < 0.1
+                       return
+                   end
+                end
+            % Check if both elements share a common endpoint    
+            elseif (NC(SortedCA(k,2),1) == NC(SortedCA(q,2),1)) && ...
+                   (NC(SortedCA(k,2),2) == NC(SortedCA(q,2),2))    
+                % Check if both elements have the same slope (and reject 
+                %    the design if so)
+                mk = (NC(SortedCA(k,2),2)-NC(SortedCA(k,1),2))/...
+                     (NC(SortedCA(k,2),1)-NC(SortedCA(k,1),1));
+                mq = (NC(SortedCA(q,2),2)-NC(SortedCA(q,1),2))/...
+                     (NC(SortedCA(q,2),1)-NC(SortedCA(q,1),1));
+                % If the same element is being compared twice, move on
+                if k == q
+                    continue
+                elseif mk == mq
+                   feasibilityScore = feasibilityScore - 0.1;
+                   if feasibilityScore < 0.1
+                       return
+                   end
+                end
+            end
+        end
+    end
+    
+    % THIRD CONSTRAINT: The entire design must be interconnected
+    startpoints = unique(SortedCA(:,1)); endpoints = unique(SortedCA(:,2));
+    startunique = setdiff(startpoints,endpoints);
+    endunique = setdiff(endpoints,startpoints);
+    if (~isempty(startunique)) || (~isempty(endunique))
+        feasibilityScore = feasibilityScore - 0.1;
+       if feasibilityScore < 0.1
+           return
+       end
+    end
+end
+
