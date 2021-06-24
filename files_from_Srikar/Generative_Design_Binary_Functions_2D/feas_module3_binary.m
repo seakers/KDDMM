ee5e339322ebb56952807057a85f3e639e45637e
sms722 <56262896+sms722@users.noreply.github.com>
2021-06-24 11:05:34 -0400

    Add files via upload

--- files_from_Srikar/Generative_Design_Binary_Functions_2D/feas_module3_binary.m
+++ files_from_Srikar/Generative_Design_Binary_Functions_2D/feas_module3_binary.m
@@ -17,54 +17,66 @@ function flowbool = feas_module3_binary(CA)
         flowbool = 1;
         return
     else
-        % Determine whether there are startpoints or endpoints that are
-        % only used once in the CA
-        usednodes = unique(SortedCA);
-        freqvec = [usednodes,histc(SortedCA(:),usednodes)];
-        singlestarts = []; singleends = [];
-        for i = 1:1:length(startunique)
-            if freqvec((freqvec(:,1) == startunique(i)),2) == 1
-                singlestarts = [singlestarts,startunique(i)];
-            end
-        end
-        for j = 1:1:length(endunique)
-            if freqvec((freqvec(:,1) == endunique(j)),2) == 1
-                singleends = [singleends,endunique(j)];
-            end
-        end
-        if ~isempty(singlestarts) && ~isempty(singleends)
-            for k = 1:1:length(singlestarts)
-                for m = 1:1:length(singleends)
-                    member = [singlestarts(k),singleends(m)];
-                    if ~ismember(member,SortedCA,'rows')
-                        flowbool = 0;
-                        return
-                    end
-                end
-            end
-        end
-        
         % For each unique startpoint, determine if connectivity exists to
-        % at least one unique endpoint
+        % all unique endpoints
+        endreached = zeros(1,length(endunique));
         for v = 1:1:length(startunique)
             mCA = SortedCA(SortedCA(:,1)==startunique(v),:);
             while size(mCA,1) < size(SortedCA,1)
                 nmCA = mCA;
+                noneleft = 1;
                 for z = 1:1:size(mCA,1)
                     tCA = SortedCA(SortedCA(:,1)== (mCA(z,2)),:);
+                    repeatrows = [];
+                    for q = 1:1:size(tCA,1)
+                        if ismember(tCA(q,:),mCA,'rows')
+                            repeatrows = [repeatrows,q];
+                        end
+                    end
+                    tCA(repeatrows,:) = [];
+                    if ~isempty(tCA)
+                        noneleft = 0;
+                    end
                     nmCA = [nmCA;tCA];
                 end
                 nmCA = unique(nmCA,'rows');
                 flowdet = ismember(endunique,nmCA(:,2));
+                % If the current unique startpoint leads to all unique
+                % endpoints, the design is intraconnected
                 if all(flowdet) == true
                     flowbool = 1;
                     return
-                elseif isempty(tCA)
-                    flowbool = 0;
-                    return
+                % If this unique startpoint only connects to one unique
+                % endpoint, this portion of the design could be isolated
+                % from the rest, depending on whether any other flowpaths
+                % terminate in the current one
+                elseif (noneleft == 1) && (sum(flowdet) == 1)
+                    usednodes = unique(nmCA);
+                    extflowdet = ismember(usednodes,SortedCA(:,2));
+                    if ~any(extflowdet)
+                        flowbool = 0;
+                        disp('here');
+                        return
+                    else
+                        endreached(flowdet==1) = endreached(flowdet==1)+1;
+                        break
+                    end
+                % If there are no longer any members in the possible
+                % flowpaths, this unique startpoint doesn't connect to all
+                % unique endpoints  
+                elseif noneleft == 1
+                    endreached(flowdet==1) = endreached(flowdet==1) + 1;
+                    break
                 end
                 mCA = nmCA;
             end
         end
+        %{
+        % If a single startpoint doesn't lead to all endpoints, determine
+        % if all startpoints cumulatively lead to all endpoints
+        if all(endreached) && any(endreached>1)
+            flowbool = 1;
+        end
+        %}
     end
 end
